39fba409499b84786b3de15a00d7f2b0e3b2fc2f
HuyVu2379 <21021491.huy@student.iuh.edu.vn>
2024-11-12 13:10:10 +0700

    initialize project

---
+++ src/main/java/iuh/movieticketapp_backend/MovieTicketAppBackendApplication.java
@@ -0,0 +1,13 @@
+package iuh.movieticketapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieTicketAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iuh/movieticketapp_backend/MovieTicketAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.movieticketapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
